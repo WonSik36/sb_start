5749f2f6c6e5a777dd7bccaf1ea2fbc360d888cb
wonsik <cws0173@gmail.com>
2019-10-25 09:44:27 +0900

    make board!

---
+++ src/main/java/board/board/dto/BoardDto.java
@@ -0,0 +1,14 @@
+package board.board.dto;
+import lombok.Data;
+
+@Data
+public class BoardDto {
+	private int boadrIdx;
+	private String title;
+	private String contents;
+	private int hitCnt;
+	private String CreatorId;
+	private String createdDatetime;
+	private String updatorId;
+	private String updatedDatetime;
+}
