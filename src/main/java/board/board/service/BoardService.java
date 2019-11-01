c1a21d350342a822ec8b3a5b77c586da4bc001ef
wonsik <cws0173@gmail.com>
2019-11-01 19:35:55 +0900

    add function that update file to server and check file info

--- src/main/java/board/board/service/BoardService.java
+++ src/main/java/board/board/service/BoardService.java
@@ -5,12 +5,12 @@ import board.board.mapper.BoardMapper;
 
 import java.util.List;
 
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public interface BoardService {
 	void setBoardMapper(BoardMapper boardMapper);
 	List<BoardDto> selectBoardList();
-	void insertBoard(BoardDto board);
+	void insertBoard(BoardDto board,MultipartHttpServletRequest multipartHttpServletRequest);
 	BoardDto selectBoardDetail(int boardIdx);
 	void updateBoard(BoardDto board);
 	void deleteBoard(int boardIdx);
--- src/main/java/board/board/service/BoardServiceImpl.java
+++ src/main/java/board/board/service/BoardServiceImpl.java
@@ -1,9 +1,15 @@
 package board.board.service;
 
+import java.util.Iterator;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import board.board.dto.BoardDto;
 import board.board.mapper.BoardMapper;
@@ -11,6 +17,7 @@ import board.board.mapper.BoardMapper;
 @Service
 public class BoardServiceImpl implements BoardService {
 	@Autowired private BoardMapper boardMapper;
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Override
 	public void setBoardMapper(BoardMapper boardMapper) {
@@ -23,8 +30,25 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
-	public void insertBoard(BoardDto board) {
-		boardMapper.insertBoard(board);
+	public void insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) {
+		//boardMapper.insertBoard(board);
+		if(ObjectUtils.isEmpty(multipartHttpServletRequest) == false) {
+			Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
+			String name;
+			while(iterator.hasNext()) {
+				name = iterator.next();
+				log.debug("file tag name: "+name);
+				List<MultipartFile> list = multipartHttpServletRequest.getFiles(name);
+				for(MultipartFile multipartFile: list) {
+					log.debug("start file information");
+					log.debug("file name: "+multipartFile.getOriginalFilename());
+					log.debug("file size: "+multipartFile.getSize());
+					log.debug("file content type: "+multipartFile.getContentType());
+					log.debug("end file information\n");
+				}
+				
+			}
+		}
 	}
 
 	@Override
