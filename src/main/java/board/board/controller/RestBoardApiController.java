01e0d53a372bf7d7b8e2fc6c0a058ce845fe8b02
wonsik <cws0173@gmail.com>
2019-11-07 09:01:05 +0900

    applying swagger

--- src/main/java/board/board/controller/RestBoardApiController.java
+++ src/main/java/board/board/controller/RestBoardApiController.java
@@ -11,38 +11,47 @@ import org.springframework.web.bind.annotation.RestController;
 
 import board.board.dto.BoardDto;
 import board.board.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
+@Api(tags="게시판 REST API")
 @RestController
 public class RestBoardApiController {
 	
 	@Autowired
 	private BoardService boardService;
 	
+	@ApiOperation(value="게시글 목록 조회")
 	@RequestMapping(value="/api/board", method=RequestMethod.GET)
 	public List<BoardDto> openBoardList() throws Exception{
 		return boardService.selectBoardList();
 	}
 	
+	@ApiOperation(value="게시글 작성")
 	@RequestMapping(value="/api/board/write", method=RequestMethod.POST)
 	public void insertBoard(@RequestBody BoardDto board){
 		boardService.insertBoard(board);
 	}
 	
+	@ApiOperation(value="게시글 상세 내용 조회")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.GET)
-	public BoardDto openBoardDetail(@PathVariable("boardIdx") int boardIdx){
+	public BoardDto openBoardDetail(@PathVariable("boardIdx") @ApiParam(value="게시글 번호") int boardIdx){
 		
 		return boardService.selectBoardDetail(boardIdx);
 	}
 	
+	@ApiOperation(value="게시글 상세 내용 수정")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.PUT)
-	public String updateBoard(@RequestBody BoardDto board, @PathVariable("boardIdx") int boardIdx){
+	public String updateBoard(@RequestBody BoardDto board, @PathVariable("boardIdx") @ApiParam(value="게시글 번호") int boardIdx){
 		board.setBoardIdx(boardIdx);
 		boardService.updateBoard(board);
 		return "redirect:/board";
 	}
 	
+	@ApiOperation(value="게시글 삭제")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.DELETE)
-	public String deleteBoard(@PathVariable("boardIdx") int boardIdx){
+	public String deleteBoard(@PathVariable("boardIdx") @ApiParam(value="게시글 번호") int boardIdx){
 		boardService.deleteBoard(boardIdx);
 		return "redirect:/board";
 	}
